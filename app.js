8dba015791f8a3cee7e7ba57a8fa9ea1ba5befca
Andrey Marev <andrey.marev@gmail.com>
2018-10-09 10:59:29 +0300

    Changed eventListener functions from arrow to anonymous

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ import {time} from './time.js';
 $('header').text(`${time.getMonth()} ${time.getYear()}`)
 render.renderMonthlyView();
   
-$('#month-btn').on('click', (e) => {
+$('#month-btn').on('click', function(e) {
     $('#tbl').children().remove();
     $('li.active').removeClass('active');
     $('#month-btn').addClass('active');
     render.renderMonthlyView();
 });
-$('#week-btn').on('click', (e) => {
+$('#week-btn').on('click', function(e) {
     $('#tbl').children().remove();
     $('li.active').removeClass('active');
     $('#week-btn').addClass('active');
     render.renderWeeklyView();
 });
-$('#day-btn').on('click', (e) => {
+$('#day-btn').on('click', function (e){
     $('#tbl').children().remove();
     $('li.active').removeClass('active');
     $('#day-btn').addClass('active');
